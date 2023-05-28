95b52d0d200f94f785e3d7b87b3b5582185de888
Dhairya Mehta <130825572+stableminded@users.noreply.github.com>
2023-05-28 17:39:58 +0530

    Image classification using Random Forest Classifier

---
+++ image__classification_project_final.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+data = pd.read_csv('mnist_train.csv')
+
+a = data.iloc[3, 1:].values
+a = a.reshape(28, 28).astype('uint8')
+plt.imshow(a)
+
+df_x = data.iloc[:, 1:]
+df_y = data.iloc[:, 0]
+
+x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=4)
+
+imputer = SimpleImputer(strategy='mean')
+x_train = imputer.fit_transform(x_train)
+x_test = imputer.transform(x_test)
+
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(x_train, y_train)
+
+pred=rf.predict(x_test)
+
+pred
+
+s=y_test.values
+count = 0
+for i in range(len(pred)):
+  if pred[i]==s[i]:
+   count+=1
+
+count
+
+len(pred)
+
+#accuracy value
+2387/2522
